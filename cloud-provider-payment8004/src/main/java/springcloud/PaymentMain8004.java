a95a34507126272491952267c09efe11da6fae0b
Administrator <695836952@qq.com>
2020-08-16 16:14:57 +0800

    使用zookeeper作为注册中心

---
+++ cloud-provider-payment8004/src/main/java/springcloud/PaymentMain8004.java
@@ -0,0 +1,19 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yanglin
+ * @version V1.0
+ * @Package com.yl.springcloud
+ * @date 2020/8/6 23:07
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/springcloud/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author yanglin
+ * @version V1.0
+ * @Package springcloud.controller
+ * @date 2020/8/13 22:41
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/zk")
+    public String paymentzk(){
+        return "springcloud with zookeeper:"+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+
+}
