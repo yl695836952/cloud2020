9eddca0f546fbf71ff76347848ac734ac204bc2c
Administrator <695836952@qq.com>
2020-08-16 17:27:58 +0800

    订单服务使用zookeeper作为注册中心

---
+++ cloud-consumerzk-order80/src/main/java/com/yl/springcloud/OrderZKMain80.java
@@ -0,0 +1,19 @@
+package com.yl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yanglin
+ * @version V1.0
+ * @Package com.yl.springcloud
+ * @date 2020/8/16 17:15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class,args);
+    }
+}
