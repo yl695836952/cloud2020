6dfd09591d718b44d264e4eb679784e53380d237
Administrator <695836952@qq.com>
2020-08-19 22:03:32 +0800

    Hystrix支付微服务构建

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yl/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,19 @@
+package com.yl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yanglin
+ * @version V1.0
+ * @Package com.yl.springcloud
+ * @date 2020/8/19 21:41
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+}
