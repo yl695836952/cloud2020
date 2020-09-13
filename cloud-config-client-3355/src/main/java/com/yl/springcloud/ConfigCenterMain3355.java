9bb0dc64b5e350ad53b2d3ce124717f8dc03361f
Administrator <695836952@qq.com>
2020-09-13 18:09:28 +0800

    消息总线 bus

--- cloud-config-client-3355/src/main/java/com/yl/springcloud/ConfigCenterMain3355.java
+++ cloud-config-client-3355/src/main/java/com/yl/springcloud/ConfigCenterMain3355.java
@@ -12,8 +12,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigCenterMain3344 {
+public class ConfigCenterMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344.class,args);
+        SpringApplication.run(ConfigCenterMain3355.class,args);
     }
 }
