97e879be398276838c2b92b285ab59061c19c27a
Administrator <695836952@qq.com>
2020-09-13 16:42:36 +0800

    config

---
+++ cloud-config-center-3344/src/main/java/com/yl/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.yl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yanglin
+ * @version V1.0
+ * @Package com.yl.springcloud
+ * @date 2020/9/12 20:55
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/yl/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.yl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yanglin
+ * @version V1.0
+ * @Package com.yl.springcloud
+ * @date 2020/9/12 20:57
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
